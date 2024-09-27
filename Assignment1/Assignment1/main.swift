07dfafcbc02aa90793aad30e7e9b7f4ccf124cd3
hadesAida <114656337+hadesAida@users.noreply.github.com>
2024-09-27 22:18:47 +0500

    Add files via upload

---
+++ Assignment1/Assignment1/main.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+print("Hello, World!")
+
+var name = "Aida"
+var surname = "Madraimova"
+
+var birthyear = 2002
+let currentyear: Int = 2024
+var isstudent: Bool = true
+var studentstatus: String = isstudent ? "I am currently a student." : "I am not a student."
+var height: Double = 1.64
+var age: Int = 21
+
+age = currentyear - birthyear
+
+var hobby = "Reading"
+var numberofhobbies: Int = 2
+var favoritenumber: Int = 22
+var ishobbycreative: Bool = false
+var hobbycreativeness: String = ishobbycreative ? "a creative" : "not a creative"
+var lifestory: String = "My name is \(name) \(surname). I am \(age) years old, born in \(birthyear). \(studentstatus) I enjoy \(hobby), which is \(hobbycreativeness) hobby. I have \(numberofhobbies) in total, and my favorite number is \(favoritenumber)."
+print(lifestory)
+
+//bonus task
+var futureGoals:String = "In the future i want to become a professional software engineer."
+lifestory += futureGoals
+print(lifestory)
+
